8ee3ab7aee0b8bf4dd7241c671c27ff5d2099a52
Vishal Lodhi <145546912+Satisfire28@users.noreply.github.com>
2026-04-21 13:38:20 +0530

    Use environment variable for server port

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ io.on('connection', (socket) => {
     console.log('User Disconnected')
   })
 })
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('server running at 3000 Port')
-})
+});
+
